d1947295b5093a95ef5a1aa3e677fdfc66f3bf28
rickey-alok <88623787+rickey-alok@users.noreply.github.com>
2023-10-17 11:46:03 +0530

    Update header.js

--- header.js
+++ header.js
@@ -6,11 +6,10 @@ header.innerHTML = `
       <a href="./pages/Markdown.html">Markdown</a>
       <a href="./pages/JSON.html">Json</a>
       <a href="./pages/MSON.html">Mson</a>
-      <a href="./pages/CURL.html">Curl</a>
       <a href="./pages/YAML.html">Yaml</a>
       <a href="./pages/HTTP-HTTPS.html">Http-Https</a>
       <a href="./pages/OpenAPI.html">OpenAPI</a>
-      <a href="./pages/OSI-Model.html">OSI-Model</a>
+      
       <div id="header-right">
         <a href="./contact.html">Contact</a>
       </div>
