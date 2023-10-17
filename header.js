27abd68f4e001ecff8bd5ad1804a11409d33dbc5
rickey-alok <88623787+rickey-alok@users.noreply.github.com>
2023-10-17 12:14:06 +0530

    Update header.js

--- header.js
+++ header.js
@@ -3,12 +3,8 @@ header.innerHTML = `
     <div>
       <a href="./index.html">Home</a>
       <a href="./gitGitHub.html">Git&GitHub</a>
-      <a href="./pages/Markdown.html">Markdown</a>
-      <a href="./pages/JSON.html">Json</a>
-      <a href="./pages/MSON.html">Mson</a>
-      <a href="./pages/YAML.html">Yaml</a>
       <a href="./openApiSpecification.html">OpenAPI</a>
-      <a href="./pages/OpenAPI.html">OpenAPI</a>
+
       
       <div id="header-right">
         <a href="./contact.html">Contact</a>
