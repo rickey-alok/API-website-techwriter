aeb3bc54597dd77435e4466a08b363988ad4920f
rickey-alok <88623787+rickey-alok@users.noreply.github.com>
2023-10-18 19:38:53 +0530

    Update header.js

--- header.js
+++ header.js
@@ -4,7 +4,7 @@ header.innerHTML = `
       <a href="./index.html">Home</a>
       <a href="./gitGitHub.html">Git&GitHub</a>
       <a href="./openApiSpecification.html">OpenAPI</a>
-
+      <a href="./map.html">Google Map API</a>
       
       <div id="header-right">
         <a href="./contact.html">Contact</a>
