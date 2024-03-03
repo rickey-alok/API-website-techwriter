613ef9e16646cb4c1730ee9ff8ec67161f786df3
alok <rickey.alok@gmail.com>
2024-03-03 18:27:32 +0530

    updated

--- header.js
+++ header.js
@@ -2,12 +2,11 @@ header.innerHTML = `
   <nav>
     <div>
       <a href="./index.html">Home</a>
-      <a href="./VSCode.html">VSCode</a>
       <a href="./MapAPI.html">API</a>
-      <a href="./gitGitHub.html">Git&GitHub</a>
-      <a href="#">Postman</a>
-      <a href="#">Jira</a>
-      <a href="#">Markdown</a>
       <a href="./openApiSpecification.html">OpenAPI</a>
-  </nav>
-`;
+      <a href="./gitGitHub.html">Git&GitHub</a>
+      <a href="./pages/gitGitHub.md">Git_GitHub</a>
+      <a href="#">Markdown</a>
+      <a href="./VSCode.html">VSCode</a>
+      </nav>
+      `;
