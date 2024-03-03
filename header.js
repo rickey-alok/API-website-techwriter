38736df492254c5283555aeed6b7882c36f5ec7c
alok <rickey.alok@gmail.com>
2024-03-03 20:41:36 +0530

    updated

--- header.js
+++ header.js
@@ -5,7 +5,7 @@ header.innerHTML = `
       <a href="./MapAPI.html">API</a>
       <a href="./openApiSpecification.html">OpenAPI</a>
       <a href="./gitGitHub.html">Git&GitHub</a>
-      <a href="./pages/gitGitHub.md">Git_GitHub</a>
+      <a href="./pages/gitGitHub.html">Git_GitHub</a>
       <a href="#">Markdown</a>
       <a href="./VSCode.html">VSCode</a>
       </nav>
